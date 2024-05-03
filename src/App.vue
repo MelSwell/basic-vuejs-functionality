c1303b783ef4b600f14fa74fc83ed5abf7717a88
Seth Cohen <smcohen82@gmail.com>
2024-05-03 11:56:47 -0400

    some modals

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>welcome to the wanzo house</p>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <h1>Sign up now for a 10% discount on your next order!</h1>
+      <p>Grab some wanzo juice for a low low price!!!</p>
+      <template v-slot:links>
+        <a href="#">sign up</a>
+        <br>
+        <a href="#">more info</a>
+      </template>
+    </Modal>
+  </div>
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <template v-slot:links>
+        <a href="#">sign up</a>
+        <br>
+        <a href="#">more info</a>
+      </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Open Modal One</button>
+  <button @click="toggleModalTwo">Open Modal Two</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data () {
+    return {
+      title: 'wanzo',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    text-align: center;
+    margin: 400px auto;
+  }
+
+  h1 {
+    color: aquamarine;
+  }
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>default content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: rgb(255, 255, 255);
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+  }
+  h1 {
+    color: blueviolet;
+  }
+  .sale {
+    background-color: rgb(186, 186, 226);
+    color: green
+  }
+</style>
